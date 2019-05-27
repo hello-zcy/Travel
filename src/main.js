9817acf0ff1ffc5845cd0fe6391483bb1b8e0a29
hello-zcy <zhou_chunyi@163.com>
2019-05-27 16:08:55 +0800

    home-header

--- src/main.js
+++ src/main.js
@@ -3,13 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  // 挂载
   router,
+  // 路由就是根据网址的不同。返回不同的内容
   components: { App },
   template: '<App/>'
 })
