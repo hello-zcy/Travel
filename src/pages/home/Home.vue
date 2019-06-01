29be78d03e2f0006bb7c2a5e93731e0050afbaa5
hello-zcy <zhou_chunyi@163.com>
2019-06-01 10:36:16 +0800

    index-swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,30 @@
 <template>
-    <div class = "root">
+    <div id = "root">
       <home-header></home-header>
+      <home-swiper></home-swiper>
+      <div>test</div>
     </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+// import axios from 'axios'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
+  },
+  data () {
+    return {
+      // swiperList:[]
+    }
+  },
+  methods: {
+    // getHomeInfo(){
+    //   axios.get('./api/index.json?city='+this.city)
+    // }
   }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrapper">
+
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img :src="item.imgUrl" class="swiper-img">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  // props:{
+  //   list:Array
+  // },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
+        }, {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg'
+        }, {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg'
+        }
+      ]
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.swiperList.length
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background : #fff
+.wrapper
+    width :100%
+    height:0
+    overflow : hidden
+    padding-bottom : 31.25%
+    background : #eeeeee
+    .swiper-img
+      width:100%
+</style>
