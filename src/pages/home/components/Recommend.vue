acbe5e8787300d5de731b2b6535574279f0e737d
hello-zcy <zhou_chunyi@163.com>
2019-06-01 16:21:44 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="recommend">
+      <div class="title">热销推荐</div>
+      <ul>
+          <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+              <!-- 一像素边宽 -->
+              <div class="item-img-wrapper">
+                  <img  class="item-img" :src="item.imgUrl">
+              </div>
+              <div class="item-info">
+                  <p class="item-title">{{item.title}}</p>
+                  <p class="item-desc">{{item.info}}</p>
+                  <button class="item-button">查看详情</button>
+              </div>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+          title: '大连亚圣海洋馆',
+          info: '浪漫大连首战，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+          title: '大连亚圣海洋馆',
+          info: '浪漫大连首战，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+          title: '大连亚圣海洋馆',
+          info: '浪漫大连首战，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+          title: '大连亚圣海洋馆',
+          info: '浪漫大连首战，浪漫的海洋主题乐园'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    display: flex
+    height: 1.9rem
+    overflow: hidden
+    flex-direction :row
+    .item-img
+       width: 1.7rem
+       height: 1.7rem
+       padding: .1rem
+    .item-info
+       flex: 1
+       padding: .1rem
+           //文字太长了可以显示为...
+       min-width: 0
+       .item-title
+          line-height : .54rem
+          font-size: .32rem
+          ellipsis()
+        .item-desc
+          line-height: .4rem
+          color: #cccccc
+          ellipsis()
+        .item-button
+          line-height: .44rem
+          margin-top: .16rem
+          background: #ff9300
+          padding:0 .2rem
+          border-radius: .06rem
+          color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="weekend">
+        <div class="title">周末去哪儿</div>
+        <ul>
+            <li
+              class="item border-bottom"
+              v-for="item of weekendList"
+              :key="item.id"
+              border-bottom
+            >
+                <div class="item-img-wrapper">
+                    <img class="item-img" :src="item.imgUrl">
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.info}}</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWekend',
+  data () {
+      return {
+        weekendList : [
+            {
+            id: '0001',
+            imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+            title: '大连亚圣海洋馆',
+            info: '浪漫大连首战，浪漫的海洋主题乐园'
+            },
+            {
+            id: '0002',
+            imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+            title: '大连亚圣海洋馆',
+            info: '浪漫大连首战，浪漫的海洋主题乐园'
+            },
+            {
+            id: '0003',
+            imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+            title: '大连亚圣海洋馆',
+            info: '浪漫大连首战，浪漫的海洋主题乐园'
+            },
+            {
+            id: '0004',
+            imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+            title: '大连亚圣海洋馆',
+            info: '浪漫大连首战，浪漫的海洋主题乐园'
+            }
+        ]
+      }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+  .title
+    line-height: .8rem
+    background: #eeeeee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    flex-direction:column
+    .item-img-wrapper
+       overflow : hidden
+       width: 100%
+       height:0
+       padding-bottom:37%
+       .item-img
+         width:100%
+    .item-info
+        padding: .1rem
+        .item-title
+            line-height :.54rem
+            font-size: .32rem
+            ellipsis()
+        .item-desc
+            line-height : .4rem
+            color:#ccc
+            ellipsis()
+</style>
