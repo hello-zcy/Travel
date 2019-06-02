48855b77f94e982fb92c3d3429e9297a3c548429
hello-zcy <zhou_chunyi@163.com>
2019-06-02 21:38:27 +0800

    city search logic

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,16 +1,71 @@
 <template>
-    <div class="search">
-        <input type="text" class="search-input" placeholder="输入城市名称或拼音">
+    <div>
+        <div class="search">
+            <input v-model="keyword" type="text" class="search-input" placeholder="输入城市名称或拼音">
+        </div>
+        <div 
+            class="search-content"
+            ref="search"
+            v-show="keyword"    
+        >
+            <ul>
+                <li v-for="item of list" :key="item.id">
+                    {{item.name}}
+                </li>
+                <li class="search-item" v-show="hasNoData">
+                    没有找到匹配数据
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+
+import Bscroll from 'better-scroll'
 export default {
-    name: 'CitySearch'
+    name: 'CitySearch',
+    props: {
+        cities: Object
+    },
+    data () {
+        return {
+            keyword:'',
+            list: [],
+            timer: null
+        }
+    },
+    computed: {
+        hasNoData() {
+            return !this.list.length
+        }
+    },
+    watch: {
+        keyword() {
+            if(this.timer){
+                clearTimeout(this.timer)
+            }
+            this.timer=setTimeout(()=>{
+                const result=[]
+                for(let o in this.cities){
+                    this.cities[o].forEach((value)=>{
+                        if(value.spell.indexOf(this.keyword)>-1||value.name.indexOf(this.keyword)>-1){
+                            result.push(value);
+                        }
+                    })
+                }
+                this.list=result
+            },100)
+        }
+    },
+    mounted() {
+        this.scroll = new Bscroll(this.$refs.search)
+    }
 }
 </script>
 
 <style lang="stylus" scoped>
+    
     @import '~styles/varibles.styl'
     .search
         height: .72rem
@@ -25,5 +80,14 @@ export default {
             border-radius : .2rem
             padding:0 .1rem
             box-sizing : border-box
-
+    .search-content
+        z-index:1
+        overflow: hidden
+        position: absolute 
+        top: 1.58rem
+        left: 0
+        right: 0
+        bottom: 0
+        background: green 
 </style>
+c
