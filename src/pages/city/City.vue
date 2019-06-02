c948269e04112f62124af9e6c8ac6cb596e9b736
hello-zcy <zhou_chunyi@163.com>
2019-06-02 10:10:06 +0800

    add alphaet

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,23 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
+        <city-alphaet></city-alphaet>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header.vue'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphaet from './components/Alphaet'
 export default {
     name: 'City',
     components: {
         CityHeader,
-        CitySearch
+        CitySearch,
+        CityList,
+        CityAlphaet
     }
 }
 </script>
---
+++ src/pages/city/components/Alphaet.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">S</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name:'CityAlphaet'
+}
+</script>
+
+<style lang="stylus" scoped>
+ @import '~styles/varibles.styl'
+
+.list
+    position :absolute
+    top: 1.58rem
+    right:0
+    bottom:0
+    width:.45rem
+    display : flex
+    flex-direction:column
+    justify-content :center
+    .item
+        line-height : .36rem
+        text-align :center
+        color: $bgColor
+</style>
